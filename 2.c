540d59965976a644f2ed87e22cc7ce661cf409ad
sanyakoliasko-stack <sanya.koliasko@gmail.com>
2026-03-24 14:11:08 +0200

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    double a, b, c, y;
+    const double a = 5.0;
+    const double b = 2.5;
+    const double c = 3.0;
 
-    printf("--- Програма для обчислення виразу (Варіант 19) ---\n");
+    double y = (a + 4 * b - c * c) / (2 * a * b * c);
 
-    printf("Введіть значення a: ");
-    scanf("%lf", &a); // Використовуємо %lf для типу double
+    printf("--- Програма для обчислення виразу ---\n");
+    printf("Константи: a = %.2f, b = %.2f, c = %.2f\n", a, b, c);
+    printf("Формула: y = (a + 4b - c^2) / (2abc)\n");
 
-    printf("Введіть значення b: ");
-    scanf("%lf", &b);
-
-    printf("Введіть значення c: ");
-    scanf("%lf", &c);
-
-    y = (a + 4 * b - c * c) / (2 * a * b * c);
-
-    printf("\n--- Результати ---\n");
-    printf("Введені дані: a = %.2f, b = %.2f, c = %.2f\n", a, b, c);
-    printf("Вираз: y = (a + 4b - c^2) / (2abc)\n");
-    printf("Результат: y = %f\n", y);
-    printf("----------------------------------------------\n");
+    printf("\n--- Результат ---\n");
+    printf("y = %d\n", y); 
+    printf("----------------------------------------------------\n");
 
     return 0;
 }
